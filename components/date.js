974a0a74dc8655f6862e2a6ab9af93b2eb329a23
Alex Ishida <alex.weber.k@gmail.com>
2023-10-14 21:50:51 +0900

    Added tutorial code

---
+++ components/date.js
@@ -0,0 +1,7 @@
+import { parseISO, format } from 'date-fns';
+
+export default function Date({ dateString }) {
+	const date = parseISO(dateString);
+	return <time dateTime={dateString}>{format(date, 'LLLL, dd, yyyy')}</time>;
+	// return <time dateTime={dateString}>{format(date, 'yyyy-MM-dd')}</time>;
+}
